1ecc51b489ebdd85534ab2d06112913544081c41
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-04-25 17:18:56 +0200

    Update Instructions.sh

--- Instructions.sh
+++ Instructions.sh
@@ -2,33 +2,34 @@
 
 set -ouex pipefail
 
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 # Config files
-curl -s https://raw.githubusercontent.com/Emblem-66/ServerBlue/refs/heads/main/ConfigFiles.sh | bash
-
-
-# RPM Fusion
-curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/rpm-fusion-repo.list | xargs -r dnf install -y
+#curl -s https://raw.githubusercontent.com/Emblem-66/ServerBlue/refs/heads/main/ConfigFiles.sh | bash
 
 # COPR repo add
-curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-repo.list | xargs -r dnf config-manager addrepo
+#curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-repo.list | xargs -r dnf config-manager addrepo
 
-# Tailscale repo add
-#dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+# Tailscale
+dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf install -y tailscale
+systemctl enable tailscaled
+rm /etc/yum.repos.d/tailscale.repo
+
+
 
 # COPR repo add
-curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-copr.list | xargs -r dnf copr enable -y
+#curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-copr.list | xargs -r dnf copr enable -y
 
 # DNF remove packages
-curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-remove.list | xargs -r dnf remove -y
+#curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-remove.list | xargs -r dnf remove -y
 
 # DNF install packages
-curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-install.list | xargs -r dnf install -y
+#curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-install.list | xargs -r dnf install -y
 
 # DNF install packages
-curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/systemd.list | xargs -r systemctl enable
+#curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/systemd.list | xargs -r systemctl enable
 
 # COPR repo remove
-curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-copr.list | xargs -r dnf copr remove -y
+#curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-copr.list | xargs -r dnf copr remove -y
 
-# Tailscale repo remove
-rm /etc/yum.repos.d/tailscale.repo
