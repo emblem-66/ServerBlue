02d64347a37a36e45e864d4cca9f96be1d183c4b
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-06-12 12:35:39 +0200

    Update Instructions.sh

--- Instructions.sh
+++ Instructions.sh
@@ -5,10 +5,10 @@ set -ouex pipefail
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Config files
-curl -s https://raw.githubusercontent.com/Emblem-66/ServerBlue/refs/heads/main/ConfigFiles.sh | bash
+#curl -s https://raw.githubusercontent.com/Emblem-66/ServerBlue/refs/heads/main/ConfigFiles.sh | bash
 
 # COPR repo add
-curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-repo.list | xargs -r dnf config-manager addrepo
+#curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-repo.list | xargs -r dnf config-manager addrepo
 
 # Tailscale
 dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
@@ -24,10 +24,10 @@ systemctl status sshd
 #curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-copr.list | xargs -r dnf copr enable -y
 
 # DNF remove packages
-curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-remove.list | xargs -r dnf remove -y
+#curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-remove.list | xargs -r dnf remove -y
 
 # DNF install packages
-curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-install.list | xargs -r dnf install -y
+#curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/dnf-install.list | xargs -r dnf install -y
 
 # DNF install packages
 #curl -sSL https://raw.githubusercontent.com/emblem-66/ServerBlue/refs/heads/main/systemd.list | xargs -r systemctl enable
